219d877267934bb0f99d469ba0092a499adeafa4
aline dos anjos <alinepinto.2021@gmail.com>
2023-04-05 22:13:40 -0300

    Iniciando JS

---
+++ script.js
@@ -0,0 +1,33 @@
+//meu primeiro comentario
+
+
+/* 
+    segundo comentario para
+    textos maiores
+    etertw5y54ys
+*/
+console.log('ola galera');
+
+//prompt("qual a sua idade?");
+
+
+
+//conhecendo e trabalhando com variaveis
+const diaDeNascimento = 23;
+let textos = "Meu dia de nascimento e";
+
+console.log(diaDeNascimento);
+console.log(textos);
+
+//concatenando variaveis
+console.log(textos + diaDeNascimento);
+
+// exemplos 
+const primeiroNome = "aline";
+const sobrenome = "anjos";
+let minhaidade = 33;
+let souEstudante = false;
+
+//imprimir
+console.log("meu nome e",primeiroNome, sobrenome," eu tenho",minhaidade," estou estudando:", souEstudante  );
+
